fec5e4369a04d34520cc107222576c924bc651d2
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:53:32 +0200

    test of SQL export

--- OpenChannelFlowRegulation/schema.sql
+++ OpenChannelFlowRegulation/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model OpenChannelFlowRegulation of the subject dataModel.OpenChannelManagement for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model OpenChannelFlowRegulation of the subject dataModel.OpenChannelManagement for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE OpenChannelFlowRegulation_type AS ENUM ('OpenChannelFlowRegulation');
-CREATE TABLE OpenChannelFlowRegulation (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, hasRegulationStructures json, hasStructuresSimulations json, id text, location json, name text, owner json, runBy text, seeAlso json, source text, type OpenChannelFlowRegulation_type);
+CREATE TABLE OpenChannelFlowRegulation (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, hasRegulationStructures JSON, hasStructuresSimulations JSON, name TEXT, owner JSON, source TEXT, type OpenChannelFlowRegulation_type);
