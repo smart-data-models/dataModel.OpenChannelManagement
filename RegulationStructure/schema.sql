442708d5ba64509577eb451862cf4ebe70619934
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:04:29 +0200

    test of SQL export

--- RegulationStructure/schema.sql
+++ RegulationStructure/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model RegulationStructure of the subject dataModel.OpenChannelManagement for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE RegulationStructure_type AS ENUM ('RegulationStructure');
-CREATE TABLE RegulationStructure (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, numberOfGates NUMERIC, numberOfSpillways NUMERIC, owner JSON, position JSON, source TEXT, tag TEXT, teleCommand BOOLEAN, type RegulationStructure_type);
+CREATE TABLE RegulationStructure (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, numberOfGates NUMERIC, numberOfSpillways NUMERIC, owner JSON, position JSON, seeAlso JSON, source TEXT, tag TEXT, teleCommand BOOLEAN, type RegulationStructure_type);
