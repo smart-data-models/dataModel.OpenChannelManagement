6ac3098825c800719436eb73ce84ece34f4ec1a6
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:04:26 +0200

    test of SQL export

--- OpenChannelSystem/schema.sql
+++ OpenChannelSystem/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model OpenChannelSystem of the subject dataModel.OpenChannelManagement for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE OpenChannelSystem_type AS ENUM ('OpenChannelSystem');
-CREATE TABLE OpenChannelSystem (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type OpenChannelSystem_type);
+CREATE TABLE OpenChannelSystem (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type OpenChannelSystem_type);
