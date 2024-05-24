6b1a225c1bd1dabf22c2e8a76ed782000a4b4577
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:28:38 +0200

    created example of code

--- OpenChannelJunction/code/code_for_using_dataModel.OpenChannelManagement_OpenChannelJunction.py
+++ OpenChannelJunction/code/code_for_using_dataModel.OpenChannelManagement_OpenChannelJunction.py
@@ -24,33 +24,38 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "OpenChannelJunction"
 subject = "dataModel.OpenChannelManagement"
-downstreamNode = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:OpenChannelJunction:downstreamNode:CBWI:21948924'}"
-attribute = "downstreamNode"
-value = downstreamNode
-# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
-print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
-
-observedBy = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:OpenChannelJunction:observedBy:GIWE:80160975'}"
-attribute = "observedBy"
-value = observedBy
-# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
-print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
-
-position = {'type': 'Property', 'value': {'distance': 160.6, 'refPoint': 'urn:ngsi-ld:OpenChannelJunction:refPoint:JXFD:60487647'}}
+position = {'distance': 160.6, 'refPoint': 'urn:ngsi-ld:OpenChannelJunction:refPoint:JXFD:60487647'}
 attribute = "position"
 value = position
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-tag = "{'type': 'Property', 'value': ''}"
-attribute = "tag"
-value = tag
+downstreamNode = "urn:ngsi-ld:OpenChannelJunction:downstreamNode:CBWI:21948924"
+attribute = "downstreamNode"
+value = downstreamNode
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+upstreamNode = "urn:ngsi-ld:OpenChannelJunction:upstreamNode:MWGU:81565938"
+attribute = "upstreamNode"
+value = upstreamNode
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+observedBy = "urn:ngsi-ld:OpenChannelJunction:observedBy:GIWE:80160975"
+attribute = "observedBy"
+value = observedBy
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
