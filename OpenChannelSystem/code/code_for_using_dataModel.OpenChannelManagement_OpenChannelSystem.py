4b988d9753e45f84cbf90f7c9e4b5a7f43e74a19
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:28:42 +0200

    created example of code

--- OpenChannelSystem/code/code_for_using_dataModel.OpenChannelManagement_OpenChannelSystem.py
+++ OpenChannelSystem/code/code_for_using_dataModel.OpenChannelManagement_OpenChannelSystem.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "OpenChannelSystem"
 subject = "dataModel.OpenChannelManagement"
-hasSubSystem = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:OpenChannelSystem:hasSubSystem:BYUP:86302765'}"
+hasSubSystem = "urn:ngsi-ld:OpenChannelSystem:hasSubSystem:BYUP:86302765"
 attribute = "hasSubSystem"
 value = hasSubSystem
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-isComposedOf = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:OpenChannelSystem:isComposedOf:UMLF:11032914'}"
+isComposedOf = "urn:ngsi-ld:OpenChannelSystem:isComposedOf:UMLF:11032914"
 attribute = "isComposedOf"
 value = isComposedOf
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-mostDownstreamPoint = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:OpenChannelSystem:mostDownstreamPoint:IXHM:68215414'}"
+mostDownstreamPoint = "urn:ngsi-ld:OpenChannelSystem:mostDownstreamPoint:IXHM:68215414"
 attribute = "mostDownstreamPoint"
 value = mostDownstreamPoint
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-mostUpstreamPoint = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:OpenChannelSystem:mostUpstreamPoint:YUHY:75075828'}"
+mostUpstreamPoint = "urn:ngsi-ld:OpenChannelSystem:mostUpstreamPoint:YUHY:75075828"
 attribute = "mostUpstreamPoint"
 value = mostUpstreamPoint
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
